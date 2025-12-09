a5810b6ba6574a8408ea2c0998663b2a59a088e6
gokhanbey <97981071+g0khanbey@users.noreply.github.com>
2025-12-09 15:15:40 +0300

    Fix the only watch videos come
    Added checks for page type and logging for subtitle URL detection.

--- content.js
+++ content.js
@@ -1,33 +1,48 @@
-let subtitleURL = null;
 
- 
-function hookPlayerResponse() {
-    const orig = Object.defineProperty;
-    Object.defineProperty(window, "ytInitialPlayerResponse", {
-        set(v) {
-            try {
-                if (v?.captions?.playerCaptionsTracklistRenderer?.captionTracks) {
-                    subtitleURL = v.captions.playerCaptionsTracklistRenderer.captionTracks[0].baseUrl;
-                }
-            } catch (e) {}
-            orig(window, "ytInitialPlayerResponse", { value: v, writable: true });
+if (!location.pathname.startsWith("/watch")) {
+    console.log("Injected.js devre dışı: video sayfası değil.");
+} else {
+
+    console.log("Injected.js aktif!");
+
+    let subtitleURL = null;
+
+    function hookPlayerResponse() {
+        const orig = Object.defineProperty;
+        Object.defineProperty(window, "ytInitialPlayerResponse", {
+            set(v) {
+                try {
+                    const tracks =
+                        v?.captions?.playerCaptionsTracklistRenderer?.captionTracks;
+                    if (tracks && tracks.length > 0) {
+                        subtitleURL = tracks[0].baseUrl;
+                        console.log("Altyazı URL bulundu:", subtitleURL);
+                    }
+                } catch (e) {}
+
+                orig(window, "ytInitialPlayerResponse", {
+                    value: v,
+                    writable: true
+                });
+            }
+        });
+    }
+
+    hookPlayerResponse();
+
+    (function(open) {
+        XMLHttpRequest.prototype.open = function(method, url) {
+            if (url.includes("timedtext")) {
+                subtitleURL = url;
+                console.log("XHR ile altyazı yakalandı:", url);
+            }
+            return open.apply(this, arguments);
+        };
+    })(XMLHttpRequest.prototype.open);
+
+    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+        if (msg === "getSubtitleUrl") {
+            sendResponse(subtitleURL);
         }
     });
 }
-hookPlayerResponse();
- 
-(function(open) {
-    XMLHttpRequest.prototype.open = function(method, url) {
-        if (url.includes("timedtext")) {
-            subtitleURL = url;
-        }
-        return open.apply(this, arguments);
-    };
-})(XMLHttpRequest.prototype.open);
-
- 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg === "getSubtitleUrl") {
-        sendResponse(subtitleURL);
-    }
-});
